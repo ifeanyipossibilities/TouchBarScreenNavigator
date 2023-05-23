2b27f5f6a82d50946ee1feefb201f3abd47f3b7a
ifeanyipossibilities <ifeanyiukpaiyi@icloud.com>
2023-05-23 18:17:36 -0400

    Touch Bar Screen Comments

---
+++ README.md
@@ -0,0 +1,3 @@
+# TouchBarScreenNavigator
+
+My Mac Book Pro 19 Screen turned blank no damage, so i decided to try using the touch bar as screen to Navigate to some App or Connect to different wifi, the app have to be added to login item, still in progress.
---
+++ TouchBarScreenNavigator/TouchScreenController.swift
@@ -0,0 +1,141 @@
+//
+//  TouchScreenController.swift
+//  TouchBarScreenNavigator
+//
+//  Created by ifeanyi on 5/23/23.
+//
+
+import Cocoa
+
+class TouchScreenController: NSWindowController {
+
+    var ScreenLocationX = 0;
+    var ScreenLocationY = 0;
+    var mouseLocation: NSPoint { NSEvent.mouseLocation }
+
+    @IBOutlet weak var UpButtonIcon: NSButtonCell!
+    @IBOutlet weak var ScrollViewImage: NSScrollView!
+    @IBOutlet weak var DownButtonicon: NSButtonCell!
+    @IBOutlet weak var LeftbuttonIcon: NSButtonCell!
+    @IBOutlet weak var RightButtonIcon: NSButton!
+    @IBOutlet weak var CurrentScreenView: NSImageCell!
+    
+    
+    
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        
+        
+//        Mouse location or mouse Pointer is hidden in the screen capture how do i locate the pointer? now just printing the cordinate doing nothing with them maybe append the cordination to the image
+        NSEvent.addLocalMonitorForEvents(matching: [.mouseMoved]) {
+                print("mouseLocation:", String(format: "%.1f, %.1f", self.mouseLocation.x, self.mouseLocation.y))
+            self.updateScreenImage()
+
+                return $0
+            }
+            NSEvent.addGlobalMonitorForEvents(matching: [.mouseMoved]) { _ in
+                print(String(format: "%.0f, %.0f", self.mouseLocation.x, self.mouseLocation.y))
+                self.updateScreenImage()
+                
+            }
+        
+        self.CurrentScreenView.image =  self.ScreenImage()
+        
+        guard var frame = self.ScrollViewImage.documentView?.frame else { return }
+
+       
+//              frame.size.width += 10
+//              frame.size.height += 10
+
+    self.ScrollViewImage.animator().magnify(toFit: frame)
+             
+        
+//        dispach thread to update screen image but this seems not to be updating
+        DispatchQueue(label: "updateScreenImageDispach").async {
+            self.updateScreenImageDispach()
+               }
+        
+        
+
+        self.UpButtonIcon.image = NSImage(named: NSImage.touchBarGoUpTemplateName)!
+        self.DownButtonicon.image = NSImage(named:  NSImage.touchBarGoDownTemplateName)!
+        self.RightButtonIcon.image = NSImage(named:  NSImage.touchBarGoForwardTemplateName)!
+        self.LeftbuttonIcon.image = NSImage(named:  NSImage.touchBarGoBackTemplateName)!
+    }
+    
+    
+    //  update the current screen image
+    func updateScreenImage(){
+            self.CurrentScreenView.image = self.ScreenImage()
+
+    }
+    
+    
+    
+    
+    //  constantly update the current screen image every 8 milliseconds
+    func updateScreenImageDispach(){
+        while true {
+            self.CurrentScreenView.image =  self.ScreenImage()
+            usleep(8000)
+        }
+    }
+    
+//    Go Left when button is tapped
+    @IBAction func GoLeft(_ sender: Any) {
+        let image =  self.ScreenImage()
+            self.ScreenLocationX -= 10
+           
+            self.ScrollViewImage.magnify(toFit: NSRect(x: CGFloat(self.ScreenLocationX), y: CGFloat(self.ScreenLocationY), width: image.size.width, height: image.size.height))
+        self.CurrentScreenView.image = image
+
+        
+    }
+//    Go right when button tapped
+    @IBAction func GoRight(_ sender: Any) {
+        let image =  self.ScreenImage()
+            self.ScreenLocationX += 10
+            self.ScrollViewImage.magnify(toFit: NSRect(x: CGFloat(self.ScreenLocationX), y: CGFloat(self.ScreenLocationY), width: image.size.width, height: image.size.height))
+        self.CurrentScreenView.image = image
+
+        
+    }
+    
+    
+//    Function to capture the current screen
+    func ScreenImage() -> NSImage{
+        let displayID = CGMainDisplayID()
+        let imageRef = CGDisplayCreateImage(displayID)
+        let image =  NSImage(cgImage: imageRef!, size: (NSScreen.main?.frame.size)!)
+        return image
+    }
+    
+//    Go Up when button tapped
+    @IBAction func GoUp(_ sender: Any) {
+        let image =  self.ScreenImage()
+            self.ScreenLocationY += 10
+            self.ScrollViewImage.magnify(toFit: NSRect(x: CGFloat(self.ScreenLocationX), y: CGFloat(self.ScreenLocationY), width: image.size.width, height: image.size.height))
+        self.CurrentScreenView.image = image
+        
+ 
+    }
+    
+//    Go Down when button tapped
+    
+    @IBAction func GoDown(_ sender: Any) {
+        let image =  self.ScreenImage()
+            self.ScreenLocationY -= 10
+            self.ScrollViewImage.magnify(toFit: NSRect(x: CGFloat(self.ScreenLocationX), y: CGFloat(self.ScreenLocationY), width: image.size.width, height: image.size.height))
+        self.CurrentScreenView.image = image
+    
+        
+    }
+    
+    
+  
+
+    
+    
+    
+    
+}
